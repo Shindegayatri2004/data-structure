15615e052c025119141e7607fcd5d77547acc530
Shindegayatri2004 <145207182+Shindegayatri2004@users.noreply.github.com>
2025-03-06 23:02:29 +0530

    ds3.cpp

--- ds3.cpp
+++ ds3.cpp
@@ -1,52 +1,59 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+class Node {
+public:
+    int vertex;
+    Node* next;
+    Node(int v) : vertex(v), next(nullptr) {}
+};
 
 class Graph {
 public:
     int V, E;
-    int matrix[100][100];      // Adjacency Matrix
-    vector<int> adjList[100];  // Adjacency List
+    int matrix[100][100];   // Adjacency Matrix
+    Node* adjList[100];     // Adjacency List using linked list
 
     // Constructor
-    Graph(int x, int y)
-     {
-        V = x;
-        E = y;
+    Graph(int v, int e) {
+        V = v;
+        E = e;
 
         // Initialize adjacency matrix to 0
         for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++)
-             {
+            for (int j = 0; j < V; j++) {
                 matrix[i][j] = 0;
             }
+            adjList[i] = nullptr; // Initialize linked list
         }
     }
 
-    // Add edge (for both matrix & list)
-    void addEdge(int x, int y)
-     {
+    // Add edge for both matrix & linked list
+    void addEdge(int x, int y) {
         if (x >= V || y >= V) {
             cout << "Invalid edge: (" << x << ", " << y << ")" << endl;
             return;
         }
 
-        // Adjacency Matrix representation
+        // Adjacency Matrix
         matrix[x][y] = 1;
         matrix[y][x] = 1;
 
-        // Adjacency List representation
-        adjList[x].push_back(y);
-        adjList[y].push_back(x);
+        // Adjacency List (Linked List Representation)
+        Node* newNodeX = new Node(y);
+        newNodeX->next = adjList[x];
+        adjList[x] = newNodeX;
+
+        Node* newNodeY = new Node(x);
+        newNodeY->next = adjList[y];
+        adjList[y] = newNodeY;
     }
 
     // Display Adjacency Matrix
-    void displayMatrix()
-     {
+    void displayMatrix() {
         cout << "\nAdjacency Matrix:\n";
         for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++)
-             {
+            for (int j = 0; j < V; j++) {
                 cout << matrix[i][j] << " ";
             }
             cout << endl;
@@ -54,23 +61,21 @@ public:
     }
 
     // Display Adjacency List
-    void displayList()
-     {
-        cout << "\nAdjacency List:\n";
-        for (int i = 0; i < V; i++)
-         {
+    void displayList() {
+        cout << "\nAdjacency List (Linked List):\n";
+        for (int i = 0; i < V; i++) {
             cout << i << " -> ";
-            for (int neighbor : adjList[i]) 
-            {
-                cout << neighbor << " ";
+            Node* temp = adjList[i];
+            while (temp) {
+                cout << temp->vertex << " ";
+                temp = temp->next;
             }
             cout << endl;
         }
     }
 };
 
-int main() 
-{
+int main() {
     int v, e;
     cout << "Enter number of vertices: ";
     cin >> v;
@@ -81,8 +86,7 @@ int main()
 
     // Taking edge input from the user
     cout << "Enter " << e << " edges (u v format):\n";
-    for (int i = 0; i < e; i++) 
-    {
+    for (int i = 0; i < e; i++) {
         int u, v;
         cin >> u >> v;
         g.addEdge(u, v);
